61dd59c196d02bc86b7d554f85ff069ab779dd1b
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-05-11 19:56:23 +0300

    feat: add cart page frontend

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -27,8 +27,39 @@ const CartScreen = {
                 price: service.price
             });
         }
-        return `<div>Cart Screen</div>
-                <div>${getCartItems().length}</div>`;
+        const cartItems = getCartItems();
+        return `
+            <div class = "content cart">
+                <div class = "cart-list">
+                    <ul class = "cart-list-container">
+                        <li>
+                            <h2>Корзина</h2>
+                            <div class = "cart-price">Цена, руб.</div>
+                        </li>
+                        ${
+                            cartItems.length === 0 ?
+                                '<div>Корзина пуста. <a href = "/#/">Выбрать услугу</a></div>' : 
+                                cartItems.map((item) => `
+                                    <li>
+                                        <div class = "cart-name">
+                                            <div>
+                                                <a href = "/#/service/${item.service}">${item.name}</a>
+                                            </div>
+                                            <button type = "button" class = "delete-button" id = "${item.servive}">Удалить</button>
+                                        </div>
+                                        <div class = "cart-price">
+                                            ${item.price}
+                                        </div>
+                                    </li>
+                                `).join('\n')
+                        }
+                    </ul>
+                </div>
+                <div class = "cart-action">
+                    <h3>Итоговая сумма: ${cartItems.reduce((a, c) => a + c.price, 0)} руб.</h3>     
+                    <button id = "checkout-button" class = "primary">Оформить заказ</button>   
+                </div>
+            </div>`;
     },
 };
 
